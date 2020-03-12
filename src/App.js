b0e174e16467ebf9e14fd3b28a01d5086bbfe0ee
Marcelo <marcelohenrique1226@outlook.com>
2020-03-11 22:01:48 -0300

    nosso primeiro commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import CardProjeto from "./components/CardProjeto";
+import ModalNovoProjeto from "./components/ModalNovoProjeto";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { lista: [
+      {nome: "Projeto 1",
+      usuario: "Joao Carlos",
+      likes: 2
+    }
+    ] };
+  }
+
+  adicionarProjeto(projeto){
+    this.setState({lista:[...this.state.lista, projeto]})
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/projetos")
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({ lista: result.data })
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="ui inverted fixed huge menu">
+          <div className="ui container">
+            <a href="index.html" className="item">CraudioFunding</a>
+            <ModalNovoProjeto adicionarProjeto={this.adicionarProjeto.bind(this)} />
+            <div className="ui icon input right menu">
+              <input type="text" placeholder="procurar projeto..." />
+              <i aria-hidden="true" className="search icon"></i>
+            </div>
+          </div>
+        </div>
+        <div className="ui stackable three column grid" id="grid-projetos">
+          {
+            this.state.lista.map(projeto => {
+              return (
+                <CardProjeto
+                  key={projeto.id}
+                  nome={projeto.nome} 
+                  usuario={projeto.usuario} 
+                  descricao={projeto.descricao} 
+                  likes={projeto.likes} 
+                />)
+            })
+          }
+        </div>
+
+      </>
+    );
+  }
+}
+export default App;
